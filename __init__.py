5a20fca095d52abecd117737741e15bacbf19362
Anton Tmur <tmur.a@numdes.com>
2022-12-07 18:43:23 +0400

    __init__ changed

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .data_structures import Stack, Queue, DijkstraQueue, AStarQueue
+from graph_animation import GraphAnimator
+
+__all__ = [
+    'Stack',
+    'Queue',
+    'DijkstraQueue',
+    'AStarQueue',
+    'GraphAnimator',
+]
